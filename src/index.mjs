224c96b1d2f9789e2781c66d1855a2d7823bdef0
Asad <PS210896@edu.summacollege.nl>
2025-04-30 10:51:19 +0200

    added way to display dashes to hide random word

--- src/index.mjs
+++ src/index.mjs
@@ -11,12 +11,24 @@ const getRandomWord = () => {
     //get random array index
     let index = Math.floor(Math.random() * wordsArray.length);
     let randomWord = wordsArray[index];
-    console.log(index, randomWord);
+
+    //for each randomWord.length display character in div
+    for (const element of randomWord) {
+        console.log(element);
+        let div = document.createElement('div');
+        div.innerHTML = "-"; //Hide the secret word with dashes
+        div.setAttribute('class', 'words'); //create class attribute and assign words
+        document.body.appendChild(div); //set the div in body
+    }
+
 }
+
 getRandomWord();
-//Hide the secret word with dashes
 
 //Limit the number of guesses to 7
+
+let guess = 7; //amount of guesses per game
+
 
 //for each guess if its in the word, reveal the letters
 
