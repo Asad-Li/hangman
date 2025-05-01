9d9f5abcd0290e0aca8f73a8e7ed8b1acadacf02
Asad <PS210896@edu.summacollege.nl>
2025-05-01 13:36:48 +0200

    Fixed guessing

--- src/index.mjs
+++ src/index.mjs
@@ -2,19 +2,17 @@ import "./styles.css";
 import wordsArray from "./words.json"; //get words
 
 //Implement all game mechanics
-let index = Math.floor(Math.random() * wordsArray.length); //get random array index
-let randomWord = wordsArray[index];
+let random = Math.floor(Math.random() * wordsArray.length); //get random array index
+let randomWord = wordsArray[random];
+const randomWordArr = randomWord.split("");
 
-for (const element of randomWord) { //for each randomWord.length display character in div
+randomWordArr.forEach((element, index) => {
     let div = document.createElement('div');
     div.innerHTML = "-"; //Hide the secret word with dashes
     div.setAttribute('class', 'words'); //create class attribute and assign words
-    div.id = randomWord.indexOf(element); //set the id of div
     document.body.appendChild(div); //set the div in body
-
-}
-
-console.log(randomWord);
+    div.id = index;
+});
 
 //Limit the number of attempts to 7
 let attempts = 7; //amount of guesses per game
@@ -23,26 +21,18 @@ document.getElementById("attempt").innerHTML = attempts;
 const checkIfUserInputMatches = () => {
     //get character input from user
     const userGuess = document.getElementById("guess").value.toUpperCase();
-
     //convert string to array
-    const randomWordArr = randomWord.split("");
 
-    console.log(randomWordArr);
-    //get character word positions
-    if (randomWordArr.includes(userGuess)) { //if character input is in the randomWord
+    randomWordArr.forEach((element, index) => {
 
-        // console.log("Contains " + userGuess);
-        //reveal the letters
-        document.getElementById(`${randomWordArr}`.indexOf(userGuess, 0).toString()).innerHTML = userGuess;
-    }
-    if (!randomWordArr.includes(userGuess)) { //if character is not in the randomWord
+        if (element === userGuess) {
+            document.getElementById(`${index}`).innerHTML = element;
+        }
+        else {
+            attempts--;
+        }
 
-        // console.log("Doesnt Contain " + userGuess);
-        attempts--; //remove one attempt
-    }
-
-    //substract one guess
-    // console.log(attempts);
+    });
 }
 
 const useOneGuess = () => {
@@ -53,5 +43,5 @@ const useOneGuess = () => {
 //event listeners
 document.getElementById("userguess").addEventListener('click', useOneGuess)
 
-
+console.log(randomWord);
 //if guesses reaches 0 game ends and user is "hanged"
