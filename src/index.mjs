bd95a3043a1e13a98c5af30f5e0d959382021458
Asad <PS210896@edu.summacollege.nl>
2025-05-02 16:11:57 +0200

    Inplemented reloadgame function, also renamed startGame to encryptWord

--- src/index.mjs
+++ src/index.mjs
@@ -4,23 +4,25 @@ import wordsArray from "./words.json"; //get words
 //Implement all game mechanics
 let random = Math.floor(Math.random() * wordsArray.length); //get random array index
 let randomWord = wordsArray[random];
-const randomWordArr = randomWord.split("");
+let randomWordArr = randomWord.split("");
 let userGuessArr = [];
 let attempts = 6; //amount of guesses per game
-document.getElementById("attempt").innerHTML = attempts;
 
-randomWordArr.forEach((element, index) => {
-    let div = document.createElement('div');
-    div.innerHTML = "-"; //Hide the secret word with dashes
-    div.setAttribute('class', 'words'); //create class attribute and assign words
-    document.body.appendChild(div); //set the div in body
-    div.id = index;
-});
+const encryptWord = () => {
+    document.getElementById("attempt").innerHTML = attempts;
+    randomWordArr.forEach((element, index) => {
+        let div = document.createElement('div');
+        div.innerHTML = "-"; //Hide the secret word with dashes
+        div.setAttribute('class', 'words'); //create class attribute and assign words
+        document.body.appendChild(div); //set the div in body
+        div.id = index;
+    });
+}
+encryptWord();
 
 const checkIfUserInputMatches = () => {
     //get character input from user
     const userGuess = document.getElementById("guess").value.toUpperCase();
-    console.log(userGuess ,userGuessArr);
     if (randomWord.includes(userGuess)) {
         randomWordArr.forEach((element, index) => {
             if (element === userGuess) {
@@ -45,40 +47,57 @@ const checkIfUserInputMatches = () => {
 const useOneGuess = () => {
     checkIfUserInputMatches()
     if (attempts < 1) {
-        console.log('hanged')
         document.getElementById("hangman").src=`/assets/6.png`;
         loseGame()
     }
-    console.log('these are the arrays im testing', userGuessArr, randomWordArr);
     if (userGuessArr.toString() === randomWordArr.toString()) {
         winGame();
     }
 }
 
 const winGame = () => {
-    const win = document.createElement('h1');
+    let win = document.createElement('h2');
     win.innerHTML = "YOU WON";
     win.style.color = "green"
+    win.setAttribute('class', 'gameStatus');
     document.body.prepend(win);
-
-    //reset game
 }
 
 const loseGame = () => {
-    const lose = document.createElement('h1');
+    let lose = document.createElement('h2');
     lose.innerHTML = "YOU LOSE";
     lose.style.color = "red"
+    lose.setAttribute('class', 'gameStatus');
     document.body.prepend(lose);
     //loop over array and set characters
     randomWordArr.forEach((element, index) => {
         document.getElementById(`${index}`).innerHTML = element;
     });
+}
 
-    //reset the game
+const reloadGame = () => {
+    document.getElementById("hangman").src=`/assets/0.png`;
+    let status = document.getElementsByClassName("gameStatus");
+    while (status.length > 0) status[0].remove(); //go over status array and remove while > 0
+    randomWordArr.forEach((element, index) => {
+        let div = document.getElementById(`${index}`);
+        div.parentNode.removeChild(div);
+    });
+    randomWordArr = [];
+    randomWord = "";
+    random = 0;
+    random = Math.floor(Math.random() * wordsArray.length); //get random array index
+    randomWord = wordsArray[random];
+    randomWordArr = randomWord.split("");
+    userGuessArr = [];
+    attempts = 6;
+    encryptWord();
+    console.log(randomWord);
 }
 
 //event listeners
 document.getElementById("userguess").addEventListener('click', useOneGuess)
+document.getElementById("reload").addEventListener('click', reloadGame)
 
 console.log(randomWord);
 
